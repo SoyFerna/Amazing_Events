46fa3b8846dbc78c8680fd1b8c313668bc9911e9
Ferna Cerdosino <SoyFerna@users.noreply.github.com>
2023-03-08 21:13:34 -0300

    :sparkles: Added js files with their respective functions to discriminate past and future events

---
+++ script/upcomingevents.js
@@ -0,0 +1,46 @@
+let currentDate = data.currentDate 
+let pastEvents = []
+let upcomingEvents = []
+let eventos = data.events
+
+for(let i = 0; i<eventos.length; i++){
+
+    if(eventos[i].date > currentDate){
+        upcomingEvents.push(eventos[i])
+    }
+/*     else{
+        pastEvents.push(eventos[i])
+    } */
+
+}
+
+
+console.log(upcomingEvents)
+
+function displayCard(arr){
+    let card = ``
+    let showCard = document.getElementById("eventcontainer")
+
+    for(i = 0; i < arr.length; i++){
+        card += 
+        ` <div class="d-flex justify-content-center flex-wrap text-center">
+        <div class="card m-2 shadow" style="width: 18rem">
+            <img src="${arr[i].image } " class="card-img-top" alt="..." height="150" />
+            <div class="card-body">
+                <h5 class="card-title">${arr[i].name}</h5>
+                <p class="card-text">
+                ${arr[i].description }
+                </p>
+            </div>
+            <div class="card-footer border-0">
+                <div class="d-flex justify-content-between">
+                    <p>${arr[i].price }</p>
+                    <a href="./details.html" class="btn amazeButton">See more...</a>
+                </div>
+            </div>
+        </div>`
+    }
+showCard.innerHTML = card; 
+}
+
+displayCard(upcomingEvents)
